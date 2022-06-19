eaab52b042ad492650650c8427e66763050a3d73
jgutierrezCSU <jesusg714@gmail.com>
2022-06-19 19:12:44 +0200

    updated home,  addded top 20 popular tv shows

--- website/views.py
+++ website/views.py
@@ -20,43 +20,94 @@ views = Blueprint("views", __name__)
 @views.route("/")
 @login_required
 def home():
-    cur_pop_movies_lst = generate_curr_popular()
-   
-    # passes user to base, display certain items if logged in, else does not display
-    return render_template("home.html", user=current_user, movie=cur_pop_movies_lst,cur_pop_movies_lst=cur_pop_movies_lst)
+    cur_pop_movies_lst = generate_curr_pop_movies()
+    cur_pop_tv_show_lst = generate_curr_pop_tv_shows()
 
-def generate_curr_popular():
-     # from tmdb to get current popular movies , tmdb updates this API daily
-    response = requests.get('https://api.themoviedb.org/3/movie/popular?api_key='+  config.api_key2 + '&language=en-US&page=1')
-    
-    cur_pop_movies_r = response.json() # store parsed json response
+    # passes user to base, display certain items if logged in, else does not display
+    return render_template(
+        "home.html",
+        user=current_user,
+        cur_pop_movies_lst=cur_pop_movies_lst,
+        cur_pop_tv_show_lst=cur_pop_tv_show_lst,
+    )
+
+
+def generate_curr_pop_movies():
+    """https://developers.themoviedb.org/3/movies/get-popular-movies"""
+    # from tmdb to get current popular movies , tmdb updates this API daily
+    response = requests.get(
+        "https://api.themoviedb.org/3/movie/popular?api_key="
+        + config.api_key2
+        + "&language=en-US&page=1"
+    )
+
+    cur_pop_movies_r = response.json()  # store parsed json response
     # list of dictionaries. each index is a dict
-    fetched_cur_pop_movie_lst = cur_pop_movies_r['results']
+    fetched_cur_pop_movie_lst = cur_pop_movies_r["results"]
     # # get index position and display attributes
-    c=0
-    for key, value in fetched_cur_pop_movie_lst[0].items():
-        print(c,":",key)
-        c+=1
-   
+    # c = 0
+    # for key, value in fetched_cur_pop_movie_lst[0].items():
+    #     print(c, ":", key)
+    #     c += 1
+
     print(len(fetched_cur_pop_movie_lst))
     # Get data from retrieved lst dict and populate with relavent data
-    cur_pop_movies_lst= []
+    cur_pop_movies_lst = []
     for movie in fetched_cur_pop_movie_lst:
         cur_movie = {
-        "id": movie["id"],
-        "category": movie["genre_ids"],
-        "title": movie["title"],
-        "release_date": movie["release_date"],
-        "overview":movie["overview"],
-        "poster_path": movie['poster_path'],
-        "default_image": "https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie-1-1-300x450.jpg",
-        "vote_average": movie["vote_average"],
-        "image": f"https://image.tmdb.org/t/p/original/{movie['poster_path']}"
-         }
+            "id": movie["id"],
+            "category": movie["genre_ids"],
+            "title": movie["title"],
+            "release_date": movie["release_date"],
+            "overview": movie["overview"],
+            "poster_path": movie["poster_path"],
+            "default_image": "https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie-1-1-300x450.jpg",
+            "vote_average": movie["vote_average"],
+            "image": f"https://image.tmdb.org/t/p/original/{movie['poster_path']}",
+        }
         cur_pop_movies_lst.append(cur_movie)
-        print(cur_pop_movies_lst[0])
+        # print(cur_pop_movies_lst[0])
 
     return cur_pop_movies_lst
+
+
+def generate_curr_pop_tv_shows():
+    """https://developers.themoviedb.org/3/tv/get-popular-tv-shows"""
+    # from tmdb to get current popular movies , tmdb updates this API daily
+    response = requests.get(
+        "https://api.themoviedb.org/3/tv/popular?api_key="
+        + config.api_key2
+        + "&language=en-US&page=1"
+    )
+
+    cur_pop_tv_shows_r = response.json()  # store parsed json response
+    # list of dictionaries. each index is a dict
+    fetched_cur_pop_tv_shows_lst = cur_pop_tv_shows_r["results"]
+    # # get index position and display attributes
+    c = 0
+    for key, value in fetched_cur_pop_tv_shows_lst[0].items():
+        print(c, ":", key)
+        c += 1
+    print(fetched_cur_pop_tv_shows_lst[7])
+    print(len(fetched_cur_pop_tv_shows_lst))
+    # Get data from retrieved lst dict and populate with relavent data
+    cur_pop_tv_shows_lst = []
+    for movie in fetched_cur_pop_tv_shows_lst:
+        cur_movie = {
+            "id": movie["id"],
+            "category": movie["genre_ids"],
+            "title": movie["name"],
+            "release_date": movie["first_air_date"],
+            "overview": movie["overview"],
+            "poster_path": movie["poster_path"],
+            "default_image": "https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie-1-1-300x450.jpg",
+            "vote_average": movie["vote_average"],
+            "image": f"https://image.tmdb.org/t/p/original/{movie['poster_path']}",
+        }
+        cur_pop_tv_shows_lst.append(cur_movie)
+        # print(cur_pop_movies_lst[0])
+
+    return cur_pop_tv_shows_lst
 
 
 def generate_random_movies():
